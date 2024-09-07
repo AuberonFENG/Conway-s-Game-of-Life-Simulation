39c2a4180ce79a116a9067fd36c6992107d8907c
Ziao FENG <144572746+AuberonFENG@users.noreply.github.com>
2024-09-07 18:47:32 +0100

    Add files via upload

---
+++ GameOfLifeSolution/GameOfLifeSolution/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System;
+using System.Windows.Forms;
+
+namespace GameOfLife
+{
+    public partial class Form1 : Form
+    {
+        private GridManager gridManager;
+        private Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            gridManager = new GridManager(50, 50);
+            timer = new Timer();
+            timer.Interval = 100;
+            timer.Tick += (s, e) => gridManager.UpdateGrid();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            gridManager.DrawGrid(e.Graphics);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            gridManager.InitGrid();
+            Invalidate();
+        }
+    }
+}
+
---
+++ GameOfLifeSolution/GameOfLifeSolution/GridManager.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Drawing;
+
+namespace GameOfLife
+{
+    public class GridManager
+    {
+        private int rows;
+        private int cols;
+        private bool[,] currentGrid;
+        private bool[,] nextGrid;
+
+        public GridManager(int rows, int cols)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            currentGrid = new bool[rows, cols];
+            nextGrid = new bool[rows, cols];
+            InitGrid();
+        }
+
+        public void InitGrid()
+        {
+            Random rand = new Random();
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    currentGrid[row, col] = rand.Next(2) == 0;
+                }
+            }
+        }
+
+        public void UpdateGrid()
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    int aliveNeighbors = CountAliveNeighbors(row, col);
+                    if (currentGrid[row, col])
+                    {
+                        nextGrid[row, col] = (aliveNeighbors == 2 || aliveNeighbors == 3);
+                    }
+                    else
+                    {
+                        nextGrid[row, col] = (aliveNeighbors == 3);
+                    }
+                }
+            }
+            Array.Copy(nextGrid, currentGrid, currentGrid.Length);
+        }
+
+        private int CountAliveNeighbors(int row, int col)
+        {
+            int count = 0;
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (i == 0 && j == 0) continue;
+
+                    int newRow = row + i;
+                    int newCol = col + j;
+                    if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols)
+                    {
+                        if (currentGrid[newRow, newCol]) count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        public void DrawGrid(Graphics g)
+        {
+            int cellSize = 10;
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    var cellColor = currentGrid[row, col] ? Brushes.Black : Brushes.White;
+                    g.FillRectangle(cellColor, col * cellSize, row * cellSize, cellSize, cellSize);
+                    g.DrawRectangle(Pens.Gray, col * cellSize, row * cellSize, cellSize, cellSize);
+                }
+            }
+        }
+    }
+}
